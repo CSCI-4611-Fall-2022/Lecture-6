bea8b69f9ab132f49185dee37cd375725518a6cc
Evan Suma Rosenberg <suma@umn.edu>
2022-09-22 11:40:17 -0500

    initial commit from lecture 4

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Lecture-6
+# Lecture 6: 2D Graphics Programming (Part 2)
+
+Detailed explanation of this code can be found in the lecture video (will be added after class).
+
+You can run the program on [GitHub pages](https://csci-4611-fall-2022.github.io/Lecture-6).
+
+## Prerequisites
+
+To work with this code, you will first need to install [Node.js 16.17.0 LTS](https://nodejs.org/en/) and [Visual Studio Code](https://code.visualstudio.com/). 
+
+## Topics
+
+- TBD
+
+## Running
+
+You will need to set up the initial project by pulling the dependencies from the node package manager with:
+
+```
+npm install
+```
+
+After that, you can compile and run a server with:
+
+```
+npm run start
+```
+
+Webpack should launch your program in a web browser automatically.  If not, you can run it by pointing your browser at `http://localhost:8080`.
+
+## Acknowledgments
+
+The ship and mine images were from the Kenney [Space Shooter Redux](https://www.kenney.nl/assets/space-shooter-redux) and [Simple Space](https://www.kenney.nl/assets/simple-space) asset packages.
+
+## License
+
+Material for [CSCI 4611 Fall 2022](https://csci-4611-fall-2022.github.io) by [Evan Suma Rosenberg](https://illusioneering.umn.edu/) is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/).
---
+++ src/SpaceMinesweeper.ts
@@ -0,0 +1,158 @@
+/* Lecture 6
+ * CSCI 4611, Fall 2022, University of Minnesota
+ * Instructor: Evan Suma Rosenberg <suma@umn.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import * as gfx from 'gophergfx'
+
+export class SpaceMinesweeper extends gfx.GfxApp
+{
+    // The graphics primitives that define objects in the scene 
+    private ship: gfx.Rectangle;
+    private star: gfx.Rectangle;
+    private mine: gfx.Rectangle;
+
+    // These transforms are "groups" that are used to hold instances
+    // of the same base object when they need to be placed in the scene
+    // multiple times. They contain an array called .children that
+    // you can iterate through to access all these objects.
+    private stars: gfx.Transform2;
+    private mines: gfx.Transform2;
+
+    // Member variable to store the current position of the mouse in
+    // normalized device coordinates.
+    private mousePosition: gfx.Vector2;
+
+    // Member variable to record the last time a mine was spawned
+    private timeSinceLastMineSpawn = 0;
+
+    constructor()
+    {
+        // The first line of any child class constructor must call
+        // the base class's constructor using the super() method. 
+        super();
+
+        // Initialize all the member variables
+        this.ship = new gfx.Rectangle();
+        this.star = new gfx.Rectangle();
+        this.mine = new gfx.Rectangle();
+        this.stars = new gfx.Transform2();
+        this.mines = new gfx.Transform2();
+        this.mousePosition = new gfx.Vector2();
+        this.timeSinceLastMineSpawn = 0;
+
+        // This parameter zooms in on the scene to fit within the window.
+        // Other options include FIT or STRETCH.
+        this.renderer.viewport = gfx.Viewport.CROP;
+    }
+
+    createScene(): void 
+    {
+        // Load the star texture to make the object a sprite
+        this.star.material.texture = new gfx.Texture('./star.png');
+
+        // Create a field of 200 star instances placed randomly throughout
+        // the scene.  Note that the Math.random() function is also used to 
+        // make them vary in size.
+        const numStars = 200;
+        for(let i=0; i < numStars; i++)
+        {
+            const starInstance = new gfx.ShapeInstance(this.star);
+            const starSize = Math.random() * 0.01;
+            starInstance.scale.set(starSize, starSize);
+            starInstance.position.set(Math.random()*2-1, Math.random()*2-1);
+            this.stars.add(starInstance);
+        }
+
+        // Load the mine texture to make the object a sprite, then scale it 
+        // to an appropriate size.
+        this.mine.material.texture =  new gfx.Texture('./mine.png');
+        this.mine.scale.set(0.12, 0.12);
+    
+        // Load the ship texture to make the object a sprite, then scale it 
+        // to an appropriate size.
+        this.ship.material.texture = new gfx.Texture('./ship.png');
+        this.ship.scale.set(0.08, 0.08);
+
+        // Add all the objects to the scene. Note that the order is important!
+        // Objects that are added later will be rendered on top of objects
+        // that are added first. This is most important for the stars; because
+        // they are in the distant background, they should be added first.
+        this.scene.add(this.stars);
+        this.scene.add(this.mines);
+        this.scene.add(this.ship);
+    }
+
+    update(deltaTime: number): void 
+    {
+        // These parameters define the motions of objects in the scene,
+        // which you will use to complete the code for this assignment.
+        // You can feel free to modify them if you want your game
+        // to have a different feel from the instructor's implementation.
+        // Note that all speed variables are scaled by deltaTime.
+        // This is important to make sure that the game plays similarly
+        // on different devices regardless of the framerate.
+        const mineSpawnInterval = .5;
+        const mineSpeed = 0.1 * deltaTime;
+
+        // Point the ship wherever the mouse cursor is located.
+        // Note that this.mousePosition has already been converted to
+        // normalized device coordinates.
+        this.ship.lookAt(this.mousePosition);
+
+        // Mine movement
+        this.mines.children.forEach((mineElem: gfx.Transform2)=>{
+
+            // This code makes the mines "home" in on the ship position
+            const mineToShip = gfx.Vector2.subtract(this.ship.position, mineElem.position);
+            mineToShip.normalize();
+            mineToShip.multiplyScalar(mineSpeed);
+            mineElem.position.add(mineToShip);
+
+        });
+
+        // Check to see if enough time has elapsed since the last
+        // mine was spawned, and if so, then call the function
+        // to spawn a new mine.
+        this.timeSinceLastMineSpawn += deltaTime;
+        if(this.timeSinceLastMineSpawn >= mineSpawnInterval)
+        {
+            this.spawnMine();
+            this.timeSinceLastMineSpawn = 0;
+        }
+    }
+
+    // When the mouse moves, store the current position of the mouse.
+    // The MouseEvent object reports mouse information in screen coordinates.
+    // We need to convert them to normalized device coordinates so that
+    // they are in the same reference frame as the objects in our scene.
+    onMouseMove(event: MouseEvent): void {
+        this.mousePosition = this.getNormalizedDeviceCoordinates(event.x, event.y);
+    }
+
+    // This function creates a new mine.  In order to prevent infinite mines,
+    // which would slow down the game, we limit the total number of mines.
+    private spawnMine(): void
+    {
+        const mineSpawnDistance = 1.25;
+        const mineLimit = 20;
+        
+        // This creates a new instance of the base mine object.
+        // Note that the Mine class extends the ShapeInstance class,
+        // so only the original object will be created in GPU memory.
+        const mineInstance = new gfx.ShapeInstance(this.mine);
+        this.mines.add(mineInstance);
+
+        // Compute a random direction ahead of the ship and then translate
+        // mine far enough away that it is outside the edge of the screen.
+        mineInstance.rotation = this.ship.rotation + (Math.random() * Math.PI / 3 - Math.PI / 6);
+        mineInstance.translateY(mineSpawnDistance);
+        
+        // If we are over the mine limit, remove the oldest one!
+        if(this.mines.children.length > mineLimit)
+        {
+            this.mines.children[0].remove();
+        }
+    }
+}
